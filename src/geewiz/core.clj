ddc3b98ee99811546c794c52c29d4efd0a2dfe94
Juho Friman <juho.friman@solita.fi>
2016-08-29 16:28:51 +0300

    Imported core from proto

--- src/geewiz/core.clj
+++ src/geewiz/core.clj
@@ -1,6 +1,38 @@
 (ns geewiz.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def handlers (atom {}))
+(def types (atom {}))
+
+(defn- arg-count [f]
+    {:pre [(instance? clojure.lang.AFunction f)]}
+    (-> f class .getDeclaredMethods first .getParameterTypes alength))
+
+(defn geewiz-handler [entity body]
+    (if (= 2 (arg-count body))
+        (swap! handlers assoc entity body)
+        (throw (IllegalArgumentException. (str "Geewiz handler must be fn of 2 args. First constraints vector second fields vector.")))))
+
+(defn reset-handlers! []
+    (do
+        (reset! handlers {})
+        (reset! types {})))
+
+(defn- filter-result [result fields]
+    (if (= fields [:all])
+        result
+        (select-keys result fields)))
+
+(defn geewiz-query [{type :type constraints :constraints fields :fields :or {fields [:all]}}]
+    (let [handler (get @handlers type)]
+        (if handler
+            (filter-result (apply handler [constraints fields]) fields)
+            (throw (IllegalArgumentException. (str "No handler for type" type))))))
+
+(defn geewiz-register-type [type description]
+    (swap! types assoc type description))
+
+(defn geewiz-types []
+    (reduce (fn [acc [k v]]
+        (if (fn? v)
+            (assoc acc k (apply v []))
+            (assoc acc k v))) {} @types))
--- test/geewiz/core_test.clj
+++ test/geewiz/core_test.clj
@@ -2,6 +2,55 @@
   (:require [clojure.test :refer :all]
             [geewiz.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn teardown [test]
+    (reset-handlers!)
+    (test)
+    (reset-handlers!))
+
+(use-fixtures :each teardown)
+
+(deftest registering-handler
+    (testing "Accepts only handlers with correct arity"
+        (is (thrown? IllegalArgumentException (geewiz-handler :animal (fn []))))))
+
+(deftest testing-query
+    (testing "throws for unknown type"
+        ; No handler for :animal
+        (is (thrown? IllegalArgumentException (geewiz-query {:type :animal}))))
+
+    (testing "executing handler via query"
+        ; just a dummy handler returning something predictable
+        (geewiz-handler :animal (fn [_ _] {:type :dog :name "Rufus"}))
+
+        (is (= "Rufus" (:name (geewiz-query {:type :animal})))))
+
+    (testing "executing handler with constraints"
+        (geewiz-handler :animal (fn [[constraintKey constraintValue] _] {:type :dog :id constraintValue :name "Rufus"}))
+
+        (is (= 123 (:id (geewiz-query {:type :animal :constraints [:id 123]})))))
+
+    (testing "executing handler with constraints and fields"
+        (geewiz-handler :animal (fn [[constraintKey constraintValue] fields] {:type :dog :id constraintValue :name "Rufus" :breed "Terrier"}))
+
+        (let [result (geewiz-query {:type :animal :constraints [:id 123] :fields [:name :breed]})]
+            (is (= #{:name :breed} (set (keys result)))))))
+
+(deftest registering-types
+    (testing "no types is no types"
+        (is (empty? (geewiz-types))))
+
+    (testing "registering static types"
+        (geewiz-register-type :dog {:breed "Breed of dog" :name "Name of dog"})
+
+        (let [types (geewiz-types) dogtype (get types :dog)]
+            (is (contains? types :dog))
+            (is (contains? dogtype :breed))
+            (is (contains? dogtype :name))))
+
+    (testing "registering dynamic types"
+        (geewiz-register-type :dog (fn [] {:breed "Breed of dog" :name "Name of dog"}))
+
+        (let [types (geewiz-types) dogtype (get types :dog)]
+            (is (contains? types :dog))
+            (is (contains? dogtype :breed))
+            (is (contains? dogtype :name)))))
